4ffbfd5f25529f3fcf38a4e73634b14d8d590b86
wendy3539 <40684391+wendy3539@users.noreply.github.com>
2018-07-21 02:27:39 +0800

    Update RulesCustomize.php

--- LINE/RulesCustomize.php
+++ LINE/RulesCustomize.php
@@ -13,6 +13,8 @@ function Rules_Operating($inputStr,$userName) {
 	    	if($userName == $item['gsx$玩家名稱']['$t'])
 	    	{
 
+				$player = $item['gsx$姓名']['$t'];
+
 	    		$序號 = $item['gsx$序號']['$t'];
 
 				if($inputStr=='判資料')
@@ -21,27 +23,27 @@ function Rules_Operating($inputStr,$userName) {
 				}
 				else if($inputStr=='判勇'||$inputStr=='判勇武' )
 				{
-				     return nomalDiceRoller("2d6".$data['feed']['entry'][$序號]['gsx$勇武加值']['$t']);		
+				     return nomalDiceRoller_Customize("2d6".$data['feed']['entry'][$序號]['gsx$勇武加值']['$t'],$inputStr,$player);		
 				}
 				else if($inputStr=='判敏'||$inputStr=='判靈敏')
 				{
-				     return nomalDiceRoller("2d6".$data['feed']['entry'][$序號]['gsx$靈敏加值']['$t']);
+				     return nomalDiceRoller_Customize("2d6".$data['feed']['entry'][$序號]['gsx$靈敏加值']['$t'],$inputStr,$player);
 				}
 				else if($inputStr=='判魅'||$inputStr=='判魅力')
 				{
-				     return nomalDiceRoller("2d6".$data['feed']['entry'][$序號]['gsx$魅力加值']['$t']);
+				     return nomalDiceRoller_Customize("2d6".$data['feed']['entry'][$序號]['gsx$魅力加值']['$t'],$inputStr,$player);
 				}
 				else if($inputStr=='判意'||$inputStr=='判意志')
 				{
-				     return nomalDiceRoller("2d6".$data['feed']['entry'][$序號]['gsx$意志加值']['$t']);
+				     return nomalDiceRoller_Customize("2d6".$data['feed']['entry'][$序號]['gsx$意志加值']['$t'],$inputStr,$player);
 				}
 				else if($inputStr=='判智'||$inputStr=='判智慧')
 				{
-				     return nomalDiceRoller("2d6".$data['feed']['entry'][$序號]['gsx$智慧加值']['$t']);
+				     return nomalDiceRoller_Customize("2d6".$data['feed']['entry'][$序號]['gsx$智慧加值']['$t'],$inputStr,$player);
 				}
 				else if($inputStr=='判精'||$inputStr=='判精神')
 				{
-				     return nomalDiceRoller("2d6".$data['feed']['entry'][$序號]['gsx$精神加值']['$t']);
+				     return nomalDiceRoller_Customize("2d6".$data['feed']['entry'][$序號]['gsx$精神加值']['$t'],$inputStr,$player);
 				}	    		
 
 	    	}
