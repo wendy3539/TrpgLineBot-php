3251feb611bec308dfb849fce847cbdca1c37725
wendy3539 <40684391+wendy3539@users.noreply.github.com>
2018-08-17 18:55:08 +0800

    Update RulesCustomize.php

--- LINE/RulesCustomize.php
+++ LINE/RulesCustomize.php
@@ -40,7 +40,7 @@ function Rules_Operating($inputStr,$userName) {
 				}	
 				else if($inputStr=='判防')
 				{
-				     return nomalDiceRoller_Customize("0d1".$data['feed']['entry'][$序號]['gsx$防禦數值']['$t'],$inputStr,$player);
+					return nomalDiceRoller_Customize("1d0-1".$data['feed']['entry'][$序號]['gsx$防禦數值']['$t'],$inputStr,$player);
 				}	  				
 				else if($inputStr=='判一')
 				{
